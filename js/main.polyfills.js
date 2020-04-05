93e24d3449a12104c674870ff61e36b05a1837d3
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-04-05 23:01:24 +0200

    Updates

---
+++ js/main.polyfills.js
@@ -0,0 +1,8 @@
+/*! project-name v0.0.1 | (c) 2020 Francuski Miroslav | MIT License | http://link-to-your-git-repo.com */
+/**
+ * Element.matches() polyfill (simple version)
+ * https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
+ */
+if (!Element.prototype.matches) {
+	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
